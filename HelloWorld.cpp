a35b93864e653c5d639a0a0d9fbe76625042360c
dave <razormayhem4z@gmail.com>
2024-03-17 14:44:29 +1100

    Test xsimd compilation on intel mac

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+#include "xsimd/xsimd.hpp"
+
+int main(int argc, char *argv[]) {
+  std::cout << "Hello, world!\n";
+
+  
+
+  return 0;
+}
---
+++ Test.h
@@ -0,0 +1,6 @@
+#include "stdlib.h"
+
+int main(int argc, const char** argv) {
+    std::cout << "Yipee" << std::endl;
+    return 0;
+}
